a7903b226a0aefe2cb90dd54a813cae49f86fbeb
Jiauyan <yanyantan0816@gmail.com>
2024-05-26 00:44:29 +0800

    setup tip image

--- src/api/v1/routes/tips.js
+++ src/api/v1/routes/tips.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const router = express.Router();
-
+const multer = require('multer');
+const path = require('path');
 const tipsController = require("../controllers/tipsController");
 
-//router.patch("//:uid", tipsController);
-//router.patch("/", tipsController);
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'src/api/v1/uploads/'); 
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
+
+// Routing
 router.get("/getAllTips", tipsController.getAllTips);
 router.get("/getAllUserTips/:uid", tipsController.getAllTipsOfUser);
 router.get("/getTipById/:id", tipsController.getTipById);
-router.post("/addTip", tipsController.addTip);
+router.post("/addTip",upload.single('tipImage'), tipsController.addTip);
 router.delete("/deleteTip/:id", tipsController.deleteTip);
-router.patch("/updateTip/:id", tipsController.updateTip);
-//router.patch("/completeGoal/:id", goalsController.completeGoal);
+router.patch("/updateTip/:id", upload.single('tipImage'), tipsController.updateTip);
+router.post("/uploadTipImage", upload.single('tipImage'), tipsController.uploadTipImage);
 
 module.exports = router;
