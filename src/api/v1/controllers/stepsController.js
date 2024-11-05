625ae9fb387773ffbe1fa8468db21686b2a27b89
Jaslene <jaslenechia39@gmail.com>
2024-11-05 21:44:45 +0800

    edited updateWater function, added set step goal functions

--- src/api/v1/controllers/stepsController.js
+++ src/api/v1/controllers/stepsController.js
@@ -24,7 +24,40 @@ const getStepsCount = async (req, res) => {
     }
 };
 
+const updateStepGoal = async (req, res) => {
+    try {
+        const { uid } = req.params;
+        const { stepGoal } = req.body;
+
+        // Log the received data for debugging
+        console.log('Received data for stepsGoal:', stepGoal);
+        const updateResponse = await stepsService.updateStepGoal(uid, stepGoal);
+
+        return res.status(200).json(updateResponse); // Use 200 for successful updates
+    } catch (err) {
+        console.error('Error in updateStepGoal:', err);
+        res.status(500).json({ message: err.message });
+    }
+};
+
+const getStepGoal = async (req, res) => {
+    try {
+        const { uid } = req.params;
+        const steps = await stepsService.getStepGoalByUid(uid);  
+        return res.status(200).json({ steps });
+    } catch (err) {
+        console.error('Error fetching step goal:', err);
+        // Check if the error is about a missing step goal
+        if (err.message.includes('No step goal found')) {
+            return res.status(404).json({ message: err.message });
+        }
+        return res.status(500).json({ message: err.message });
+    }
+};
+
 module.exports = {
     storeStepsCount,
-    getStepsCount
+    getStepsCount,
+    updateStepGoal,
+    getStepGoal
 }
