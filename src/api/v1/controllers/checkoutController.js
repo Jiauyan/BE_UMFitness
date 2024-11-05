72d526886816a694cfdb3120f2eddb860a8f9b95
Jiauyan <yanyantan0816@gmail.com>
2024-11-05 22:57:18 +0800

    update refund

--- src/api/v1/controllers/checkoutController.js
+++ src/api/v1/controllers/checkoutController.js
@@ -16,12 +16,39 @@ const createCheckoutSession = async (req, res) => {
         customerName
       });
 
-      res.status(200).json({ sessionId: session.id });
+      res.status(200).json({ sessionId: session.id , session: session});
   } catch (err) {
       res.status(500).json({ message: err.message });  // Changed status code to 500 to reflect server errors
   }
 };
 
+const completeCheckout = async (req, res) => {
+  try {
+      const { sessionId } = req.body;  // Correctly extract sessionId directly from req.body
+      const session = await checkoutService.completeCheckout(sessionId);  // Pass the sessionId correctly
+
+      res.status(200).json({ session });
+  } catch (err) {
+      console.log(err);  // Good practice to log the actual error for better debugging
+      res.status(500).json({ message: err.message });
+  }
+};
+
+const refundPayment = async (req, res) => {
+  try {
+      const { paymentIntentId } = req.body; // Extract payment intent ID from request body
+      if (!paymentIntentId) {
+          return res.status(400).json({ message: 'Payment Intent ID is required' });
+      }
+      const refund = await checkoutService.refundPayment(paymentIntentId);
+      res.status(200).json({ message: 'Refund processed successfully', refund });
+  } catch (error) {
+      res.status(500).json({ message: 'Failed to process refund', error: error.message });
+  }
+};
+
 module.exports = {
-    createCheckoutSession
+    createCheckoutSession,
+    completeCheckout,
+    refundPayment
 };
--- src/api/v1/routes/checkout.js
+++ src/api/v1/routes/checkout.js
@@ -5,5 +5,8 @@ const checkoutController = require("../controllers/checkoutController");
 
 
 router.post("/createCheckoutSession", checkoutController.createCheckoutSession);
+router.post("/completeCheckout", checkoutController.completeCheckout);
+router.post("/refundPayment", checkoutController.refundPayment);
+
 
 module.exports = router;
