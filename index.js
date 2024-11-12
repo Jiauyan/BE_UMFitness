344a2886d7956201e4c1630b0ed044927f28ba5e
Jiauyan <yanyantan0816@gmail.com>
2024-11-12 16:33:25 +0800

    update vercel

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-// Only configure dotenv if not in production
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-  }
-  
-  require('./src/server');
+require('dotenv').config();
+require('./src/server');
